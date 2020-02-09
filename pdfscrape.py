b31d8b3b3dac724ca004c719abe466437350afdb
Connor Tremblay <ctrembl1@binghamton.edu>
2020-02-08 21:15:21 -0500

    Commit pdfscrape

--- pdfscrape.py
+++ pdfscrape.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import re
+import os
 #from pdf_annotate import PdfAnnotator, Appearance, Location
 
 class Scraped_PDF(object):
@@ -7,22 +8,67 @@ class Scraped_PDF(object):
         self.path = path
         self.scrape_pdf()
 
-    def scrape_pdf(self, path):
-        pdfFileObject = open(path, 'rb')
+    def scrape_pdf(self):
+        pdfFileObject = open(os.getcwd()+self.path, 'rb')
+        print(PyPDF2.PdfFileReader(pdfFileObject).getPage(0).extractText())
         pdf_info = PyPDF2.PdfFileReader(pdfFileObject).getPage(0).extractText().split('\n')
-        self.company_name = pdf_info[2]
-        self.transaction_type = pdf_info[pdf_info.index('Trans.Type') + 2]
-        q = pdf_info[pdf_info.index('Quantity') + 2].replace(',', '')
-        pps = pdf_info[pdf_info.index('PPS') + 2]
-        self.quantity = int(q) if q else None
-        self.price_per_share = int(pps[1:]) if pps else None
-        self.date_of_order = pdf_info[pdf_info.index('Date of Order: ') + 1]
-        self.custodian = pdf_info[pdf_info.index('Custodian: ') + 1]
-        self.security = pdf_info[pdf_info.index('Security') + 2]:
-        self.type = pdf_info[pdf_info.index('Type of ') + 2]:
-        self.broker_dealer = pdf_info[pdf_info.index('Broker/Dealer') + 2]:
-        self.phone = pdf_info[pdf_info.index('Phone:') + 2]:
-        self.ammount = quantity * price_per_share
+        print(pdf_info)
+        try:
+            self.company_name = pdf_info[2]
+        except:
+            self.company_name = None
+        
+        try:
+            self.transaction_type = pdf_info[pdf_info.index('Trans Type') + 2]
+        except:
+            self.transaction_type = None
+        
+        try:
+            self.quantity = int(pdf_info[pdf_info.index('Quantity') + 2].replace(',', '')[1:])
+        except:
+            self.quantity = None
+
+        try:
+            self.price_per_share = int(pdf_info[pdf_info.index('PPS') + 2][1:])
+        except:
+            self.price_per_share = None
+
+        try:
+            self.date_of_order = pdf_info[pdf_info.index('Date of Order: ') + 1]
+        except:
+            self.date_of_order = None
+    
+        try:
+            self.custodian = pdf_info[pdf_info.index('Custodian: ') + 1]
+        except:
+            self.custodian = None
+
+        try:
+            self.security = pdf_info[pdf_info.index('Security') + 2]
+        except:
+            self.security = None
+
+        try:
+            self.type = pdf_info[pdf_info.index('Type of ') + 2]
+        except:
+            self.type = None
+        
+        try:
+            self.broker_dealer = pdf_info[pdf_info.index('Broker/Dealer') + 2]
+        except:
+            self.broker_dealer = None
+        
+        try:
+            self.phone = pdf_info[pdf_info.index('Phone:') + 2]
+        except:
+            self.phone = None
+        
+        try:
+            self.amount = quantity * price_per_share
+        except:
+            self.amount = None
+        
+        self.valid_pdf = self.amount != None and self.transaction_type != None and self.quantity != None
         pdfFileObject.close()
 
 
@@ -31,15 +77,16 @@ class trade_ticket(object):
     def __init__(self, obj):
         self.company_name = obj.company_name
         self.transaction_type = obj.transaction_type
-        self.ammount = self.ammount
-
+        self.amount = obj.amount
     
     def __str__(self):
-        return f"Company name: {self.company_name}\nTransaction Type: {self.transaction_type}\nAmmount: {self.ammount}\n"
+        return f'Company name: {self.company_name}\nTransaction Type: {self.transaction_type}\nAmount: {self.amount}\n'
 
 
 if __name__ == "__main__":
-    alex = trade_ticket('trade1.pdf')
+    spdf = Scraped_PDF('/trade_tickets/CompanyA_Q2_p3.pdf')
+    alex = trade_ticket(spdf)
+    print(alex)
 
 
 
